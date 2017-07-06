9f6fd02350e6ee13c4c78d54146a9f7d3e115cbe
Francois Aguet <francois@broadinstitute.org>
2017-07-06 15:32:11 -0400

    More efficient implementation

--- rnaseq/src/combine_GCTs.py
+++ rnaseq/src/combine_GCTs.py
@@ -9,6 +9,7 @@ import os
 parser = argparse.ArgumentParser(description='Combine GCT files')
 parser.add_argument('input_files', nargs='+', help='List of GCT files, or file containing paths to GCTs.')
 parser.add_argument('output_prefix', help='Prefix for output file: <output_prefix>.gct.gz')
+parser.add_argument('--hdf_key', default=None)
 parser.add_argument('-o', '--output_dir', default='.', help='Output directory')
 args = parser.parse_args()
 
@@ -19,14 +20,31 @@ else:
     paths = args.input_files
 
 sample_ids = np.array([os.path.split(i)[1].split('.')[0] for i in paths])
+assert len(sample_ids)==len(np.unique(sample_ids))
+# sort by sample_id
 i = np.argsort(sample_ids)
 sample_ids = sample_ids[i]
 paths = np.array(paths)[i]
 
-# sort by sample_id
-gct_df = [pd.read_csv(paths[0], sep='\t', skiprows=3, header=None, index_col=0, names=['Name','Description', sample_ids[0]])]
-gct_df += [pd.read_csv(i, sep='\t', skiprows=3, header=None, usecols=[0,2], index_col=0, names=['Name','Description',j]) for i,j in zip(paths[1:], sample_ids[1:])]
-gct_df = pd.concat(gct_df, axis=1)
+df = pd.read_csv(paths[0], sep='\t', skiprows=3, header=None, index_col=0, names=['Name','Description', sample_ids[0]])
+if df[sample_ids[0]].dtype==np.float64:
+    dtype = np.float32
+elif df[sample_ids[0]].dtype==np.int64:
+    dtype = np.int32
+else:
+    dtype = df[sample_ids[0]].dtype.type
+
+gct_df = pd.DataFrame(0, index=df.index, columns=['Description']+list(sample_ids), dtype=dtype)
+gct_df['Description'] = df['Description']
+gct_df[sample_ids[0]] = df[sample_ids[0]]
+for k,(i,p) in enumerate(zip(sample_ids[1:], paths[1:])):
+    print('\rProcessing {}/{}'.format(k+2, len(paths)), end='', flush=True)
+    df = pd.read_csv(p, sep='\t', skiprows=3, header=None, usecols=[0,2], index_col=0, names=['Name',i], dtype={'Name':str, i:dtype})
+    gct_df[i] = df[i]
+print()
+
+if args.hdf_key:
+    gct_df.to_hdf(os.path.join(args.output_dir, args.output_prefix+'.hdf'), args.hdf_key)
 
 with gzip.open(os.path.join(args.output_dir, args.output_prefix+'.gct.gz'), 'wt') as f:
     f.write('#1.2\n')
