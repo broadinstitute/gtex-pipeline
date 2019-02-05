bfe359330bac223254e0332be1ed4b5eafebbd9d
François Aguet <francois@broadinstitute.org>
2019-02-05 12:07:54 -0500

    Added option

--- rnaseq/src/run_MarkDuplicates.py
+++ rnaseq/src/run_MarkDuplicates.py
@@ -23,6 +23,7 @@ parser.add_argument('-o', '--output_dir', default=os.getcwd(), help='Output dire
 parser.add_argument('-m', '--memory', default=3, type=int, help='Memory, in GB')
 parser.add_argument('--max_records_in_ram', default=500000, type=int,
                     help='Number of records stored in RAM before spilling to disk')
+parser.add_argument('--sorting_collection_size_ratio', default=0.25, type=float)
 parser.add_argument('--optical_duplicate_pixel_distance', default=100,
                     help='Maximum offset between two duplicate clusters. 100 (default) is appropriate for unpatterned, 2500 recommended for patterned flowcells.')
 parser.add_argument('--jar', default='/opt/picard-tools/picard.jar', help='Path to Picard jar')
@@ -39,6 +40,7 @@ with cd(args.output_dir):
         +' O={}'.format(os.path.basename(args.input_bam).replace('.bam', '.md.bam'))\
         +' PROGRAM_RECORD_ID=null'\
         +' MAX_RECORDS_IN_RAM={}'.format(args.max_records_in_ram)\
+        +' SORTING_COLLECTION_SIZE_RATIO={}'.format(args.sorting_collection_size_ratio)\
         +' TMP_DIR={}'.format(args.output_dir)\
         +' M={}.marked_dup_metrics.txt ASSUME_SORT_ORDER=coordinate OPTICAL_DUPLICATE_PIXEL_DISTANCE={}'.format(args.prefix, args.optical_duplicate_pixel_distance), shell=True)
 
