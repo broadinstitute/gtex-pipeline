6e42e9e7b92c57c6937a0ee09b788061189adb47
francois-a <francois@broadinstitute.org>
2024-04-06 07:03:47 +0000

    Retain strand information; parquet option

--- rnaseq/src/process_star_junctions.py
+++ rnaseq/src/process_star_junctions.py
@@ -3,11 +3,13 @@ import numpy as np
 import argparse
 import os
 import gzip
+import qtl.io
 
 parser = argparse.ArgumentParser(description='Convert STAR junction output (unique mapping reads) to GCT.')
 parser.add_argument('star_junction_output', help='SJ.out.tab from STAR')
 parser.add_argument('reference_junctions', help='File (tsv) containing columns: chr, intron_start, intron_end, gene_id')
 parser.add_argument('prefix', help='Prefix for output file: <prefix>.SJ.gct.gz')
+parser.add_argument('--parquet', action='store_true', help='Write to parquet format instead of GCT')
 parser.add_argument('-o', '--output_dir', default='.', help='Output directory')
 args = parser.parse_args()
 
@@ -32,23 +34,27 @@ args = parser.parse_args()
 
 # read STAR output
 columns = ['chr', 'intron_start', 'intron_end', 'strand', 'motif', 'status', 'n_unique', 'n_multi', 'max_overhang']
-dtype = {'chr':str, 'intron_start':np.int32, 'intron_end':np.int32, 'strand':np.int32, 'motif':np.int32, 'status':np.int32, 'n_unique':np.int32, 'n_multi':np.int32, 'max_overhang':np.int32}
+dtype = {'chr':str, 'intron_start':np.int32, 'intron_end':np.int32, 'strand':np.int32, 'motif':np.int32,
+         'status':np.int32, 'n_unique':np.int32, 'n_multi':np.int32, 'max_overhang':np.int32}
 junctions_df = pd.read_csv(args.star_junction_output, sep='\t', header=None, names=columns, dtype=dtype)
-junctions_df.index = junctions_df['chr']+'_'+junctions_df['intron_start'].astype(str)+'_'+junctions_df['intron_end'].astype(str)
+junctions_df['strand'] = junctions_df['strand'].map({0: '?', 1:'+', 2:'-'})
+junctions_df.index = (junctions_df['chr'] + ':' + junctions_df['intron_start'].astype(str)
+                      + '-' + junctions_df['intron_end'].astype(str) + ':' + junctions_df['strand'])
 
 # read reference
-reference_df = pd.read_csv(args.reference_junctions, sep='\t', dtype={'chr':str, 'intron_start':np.int32, 'intron_end':np.int32, 'gene_id':str})
-reference_df.index = reference_df['chr']+'_'+reference_df['intron_start'].astype(str)+'_'+reference_df['intron_end'].astype(str)
+reference_df = pd.read_csv(args.reference_junctions, sep='\t',
+                           dtype={'chr':str, 'intron_start':np.int32, 'intron_end':np.int32, 'gene_id':str})
+reference_df.index = (reference_df['chr'] + ':' + reference_df['intron_start'].astype(str)
+                      + '-' + reference_df['intron_end'].astype(str) + ':' + reference_df['strand'])
+assert not reference_df.index.duplicated().any(), "Junction annotation must not contain any duplicated entries"
 
-# only consider unique-mapping reads
-reference_df['n_unique'] = 0
-idx = np.intersect1d(reference_df.index, junctions_df.index)
-reference_df.loc[idx, 'n_unique'] = junctions_df.loc[idx, 'n_unique']
-
-# save as GCT
-gct_df = reference_df[['gene_id', 'n_unique']].rename(columns={'gene_id':'Description', 'n_unique':args.prefix})
+# use unique-mapping reads
+gct_df = reference_df[['gene_id']].join(junctions_df['n_unique'])
+gct_df['n_unique'] = gct_df['n_unique'].fillna(0).astype(int)
+# write as GCT
+gct_df.rename(columns={'gene_id':'Description', 'n_unique':args.prefix}, inplace=True)
 gct_df.index.name = 'Name'
-with gzip.open(os.path.join(args.output_dir, args.prefix+'.SJ.gct.gz'), 'wt') as gct:
-    gct.write('#1.2\n')
-    gct.write('{0:d}\t{1:d}\n'.format(gct_df.shape[0], 1))
-    gct_df.to_csv(gct, sep='\t')
+if args.parquet:
+    gct_df.to_parquet(os.path.join(args.output_dir, f"{args.prefix}.SJ.parquet"))
+else:
+    qtl.io.write_gct(gct_df, os.path.join(args.output_dir, f"{args.prefix}.SJ.gct.gz"))
